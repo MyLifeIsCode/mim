6565c6c2f5e11901aa92ec9fd621e550c4d82123
qll <249180439@qq.com>
2019-02-16 19:14:29 +0800

    edit

--- mim-server/src/main/java/mim/server/service/MsgService.java
+++ mim-server/src/main/java/mim/server/service/MsgService.java
@@ -1,6 +1,11 @@
 package mim.server.service;
 
+import mim.server.annotation.Handeler;
 import mim.server.vo.Session;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ApplicationObjectSupport;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -12,7 +17,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * @desc
  **/
 @Component
-public class MsgService {
+public class MsgService extends ApplicationObjectSupport implements SmartInitializingSingleton {
 
     private Map<Long,Session> sessionMap = new ConcurrentHashMap<>();
 
@@ -22,5 +27,11 @@ public class MsgService {
 
     public Session getSession(Long uid){
         return sessionMap.get(uid);
+    }
+
+    @Override
+    public void afterSingletonsInstantiated() {
+        ApplicationContext applicationContext = getApplicationContext();
+        Map<String, Object> beansWithAnnotation = applicationContext.getBeansWithAnnotation("Handeler.class");
     }
 }
